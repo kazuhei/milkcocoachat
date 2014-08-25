2c83f256e4f6075a413821e39679a341156fc506
kazuhei_arai <kazuhei_arai@dwango.co.jp>
2014-08-25 12:23:35 +0900

    ユーザー名登録用のページ追加
    phpinfoファイルの追加

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <style type="text/css">
+        body {
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <form method="get" action="chat.html">
+            <label>ユーザー名</label>
+            <input name="username" type="text" required>
+            <input type="submit" value="チャットをはじめる">
+        </form>
+</body>
+</html>
+
